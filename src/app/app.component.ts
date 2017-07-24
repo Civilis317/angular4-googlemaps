530b0fb20f2b2ba9ae9525608d25dc2f140a65ab
Civilis <civilis@acme.com>
2017-07-24 09:52:59 +0200

    Added Bootstrap menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
   title: string = 'BMS 3, PoC - Google Maps API';
+
+  menu = {
+    "menuItemList": [
+      {"url": "/earthquakes", "name": "Earthquakes"}
+      , {"url": "/map/1", "name": "Schagerbrug"}
+      , {"url": "/map/2", "name": "Amsterdamsebrug"}]
+    , "displayName": "Not logged in"
+    , "authenticated": false
+  };
+
+  menujson = JSON.stringify(this.menu.menuItemList);
+
 }
